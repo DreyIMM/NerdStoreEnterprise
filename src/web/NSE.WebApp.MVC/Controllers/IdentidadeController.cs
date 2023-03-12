62ed9be5e5c90b5d3c4f09d61bcaa7f1b566590e
Andrey Dario <andreydario26@gmail.com>
2023-03-12 14:59:07 -0300

    Models, Views e Controller de login

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,12 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
         {
             return View();
         }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if(!ModelState.IsValid) return View(usuarioRegistro);
+
+            //API - Reigstro
+
+            if (false) return View(usuarioRegistro);
+
+            //Realizar o login na App
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //API - Login
+
+            if (false) return View(usuarioLogin);
+
+            //Realizar o login na App
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            //limpar os cookies
+            return RedirectToAction("Index", "home");
+        }
+
     }
 }
