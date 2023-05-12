320f4d4d83a650bcdd3203b779a5d86d002a559c
Andrey Dario <andreydario26@gmail.com>
2023-05-11 21:18:43 -0300

    Melhorias na WebApp

--- src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NSE.WebApp.MVC.Extensions;
+using System.Globalization;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -39,6 +41,15 @@ namespace NSE.WebApp.MVC.Configuration
             app.UseIdentityConfiguration();
 
             app.UseMiddleware<ExceptionMiddleware>();
+
+            var supportedCultures = new[] { new CultureInfo("pt-BR") };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("pt-BR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
+
 
             app.UseEndpoints(endpoints =>
             {
