b60f454a255b185f982e7aca032baa8fabcb7992
Andrey Dario <andreydario26@gmail.com>
2023-07-02 00:21:57 -0300

    Reqiest Response Pattern

---
+++ src/building blocks/NSE.Core/Messages/Integration/UsuarioRegistradoIntegrationEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NSE.Core.Messages.Integration
+{
+    public abstract class UsuarioRegistradoIntegrationEvent : Event
+    {
+
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        protected UsuarioRegistradoIntegrationEvent(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+
+}
